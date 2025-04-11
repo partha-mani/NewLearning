814b8fd580bac44d06f7604b65db6a9a85e0a56c
partha-mani <32617127+partha-mani@users.noreply.github.com>
2025-04-11 15:34:28 +0100

    Create Account.java

---
+++ src/main/java/Account.java
@@ -0,0 +1,25 @@
+public abstract class Account implements Transaction {
+    private String accountHolder;
+    private String accountNumber;
+    protected double balance;
+
+    public Account(String accountHolder, String accountNumber, double balance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public abstract void displayAccountType();
+}

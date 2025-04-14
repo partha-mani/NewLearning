f3641b843a37ff24a4bb839efec7b3fa56170bba
parthasarathy <40832674+ParthaMani@users.noreply.github.com>
2025-04-14 14:26:53 +0100

    Mycode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         SavingsAccount myAccount = new SavingsAccount("Alice", "SA123", 1000.0);
@@ -6,9 +8,14 @@ public class Main {
         System.out.println("Account Holder: " + myAccount.getAccountHolder());
         System.out.println("Account Number: " + myAccount.getAccountNumber());
         System.out.println("Initial Balance: $" + myAccount.getBalance());
-
-        myAccount.deposit(500);     // Interface method
-        myAccount.withdraw(200);    // Interface method
+        Scanner input_deposit = new Scanner(System.in);
+        System.out.println("Please enter the deposit amount: ");
+            double deposit_amount = input_deposit.nextDouble();
+        myAccount.deposit(deposit_amount);     // Interface method
+        Scanner input_withdraw = new Scanner(System.in);
+        System.out.println("Please enter the deposit amount: ");
+        double withdraw_amount = input_withdraw.nextDouble();
+        myAccount.withdraw(withdraw_amount);    // Interface method
         System.out.println("Final Balance: $" + myAccount.getBalance());
     }
 }
